2f69e1674d905b28c2c99dd60b0e610547a1a0a8
Ken C <zhikang.chen@georgebrown.ca>
2022-02-16 18:14:59 -0500

    Change the method of detecting items from collision to sphere trace
    - Move Pickup class items to their own folder
    - Remove code for collision check
    - Added sphere trace to player's interaction
    - Remove unnecessary code in PickupItem class
    - Change the collision presets of all pickup class to dynamic
    
    
    Former-commit-id: 605eb1062b0ba23d014611714dbf443928192549

--- Source/Terratopia/TerratopiaPickupItem.cpp
+++ Source/Terratopia/TerratopiaPickupItem.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd0dbc7decd6490d773cd6687ed988b41e6e6e660a923421640fdaa076c61ce2
-size 2199
+oid sha256:735c6d01057f4bd483fae4e9da5726b541d51cd6dbced66c03693c236407a589
+size 878
--- Source/Terratopia/TerratopiaPickupItem.h
+++ Source/Terratopia/TerratopiaPickupItem.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9285b0de82ee64f8192b92b1d9b25952bfc2a69e06df141c0104cb74bf94f00
-size 1580
+oid sha256:dc7e93f76aa20b6de33e8643215cde5d43d4627b1b595aaaeb68e7a1b6ebf5f4
+size 1065
